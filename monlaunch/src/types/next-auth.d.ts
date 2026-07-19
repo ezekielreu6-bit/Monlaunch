a1824a00c552c0ac601b5e3632f34f1db5eab5d8
ezekielreu6-bit <ezekielreu6@gmail.com>
2026-07-19 22:21:59 +0100

    Update next-auth.d.ts

--- monlaunch/src/types/next-auth.d.ts
+++ monlaunch/src/types/next-auth.d.ts
@@ -1,7 +1,18 @@
-import { DefaultUser } from "next-auth";
+import { DefaultSession, DefaultUser } from "next-auth";
+import { JWT as DefaultJWT } from "next-auth/jwt";
 
 declare module "next-auth" {
   interface User extends DefaultUser {
     address: string;
+  }
+
+  interface Session extends DefaultSession {
+    address?: string;
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT extends DefaultJWT {
+    address?: string;
   }
 }
